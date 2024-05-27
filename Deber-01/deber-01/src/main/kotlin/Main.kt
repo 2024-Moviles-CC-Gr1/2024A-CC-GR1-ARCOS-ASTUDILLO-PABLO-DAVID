3846dbacae5c030c76219b60e3728eeb97ef0461
Pablin72 <127166642+Pablin72@users.noreply.github.com>
2024-05-27 18:55:21 -0500

    Delete Deber-01/deber-01 directory

--- Deber-01/deber-01/src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-class Pais {
-    val nombre: String
-    val numeroTotalIngredientes: Int
-    val superficie: Double
-    val esIndependiente: Boolean
-    val fechaFundacion: String,
-    val ciudades: MutableList<Ciudad> = mutableListOf()
-}
